c5655c84944e2343564544404c25fd84aefb7c09
itsujwal2019 <124653970+itsujwal2019@users.noreply.github.com>
2023-06-10 11:54:03 +0545

    Delete user function

--- Marketplace/accounts/urls.py
+++ Marketplace/accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from rest_framework import routers
-from Marketplace.accounts.views import registration_view, login_view, get_user_profile
+from Marketplace.accounts.views import registration_view, login_view, get_user_profile, delete_user_profile
 
 urlpatterns = [
     path('api/register/', registration_view, name='register'),
     path('api/login/', login_view, name='login'),
     path('api/get_user_profile/', get_user_profile, name='get_user_profile'),
+    path('api/delete_user_profile/', delete_user_profile, name='delete_user_profile'),
 ]
